0eef6640f646360708a01a8e5a4e7a3f1908a1c3
Corruptbyte <130069761+Corruptbyte@users.noreply.github.com>
2023-11-30 16:17:26 -0600

    Update app.js

--- Dad Jokes API/app.js
+++ Dad Jokes API/app.js
@@ -1,25 +1,21 @@
+// Selecting the button and paragraph elements
 const button = document.querySelector('.container button');
 const jokeText = document.querySelector('.container p');
-document.addEventListener('DOMContentLoaded', getJoke)
 
+// Event listener for when the page is loaded
+document.addEventListener('DOMContentLoaded', getJoke);
+
+// Event listener for when the button is clicked
 button.addEventListener('click', getJoke);
 
-// async function getJoke(){
-// const jokeData = await fetch ('https://icanhazdadjoke.com/', {
-// headers: {
-// 'Accept': 'application/json'
-//     }
-//   })
-// const jokeObj = await jokeData.json();
-// jokeText.innerHTML = jokeObj.joke
-// }
-
-
+// Function to fetch and display a joke
 function getJoke(){
-fetch ('https://icanhazdadjoke.com/',{
-headers: {
-'Accept': 'application/json'
-    }
-  }).then(data=> data.json())
-    .then(obj => jokeText.innerHTML = obj.joke)
+    // Fetching the joke data from the API
+    fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(data => data.json()) // Parsing the response as JSON
+    .then(obj => jokeText.innerHTML = obj.joke); // Displaying the joke in the paragraph element
 }
